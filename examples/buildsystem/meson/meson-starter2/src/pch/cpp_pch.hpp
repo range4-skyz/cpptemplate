7c753135e984ebb28ebbd1a04c83249cfeb65881
range4-skyz <sxy2367221250@163.com>
2025-06-15 23:38:41 +0800

    examples: update a pch file in meson-starter2

--- examples/buildsystem/meson/meson-starter2/src/pch/cpp_pch.hpp
+++ examples/buildsystem/meson/meson-starter2/src/pch/cpp_pch.hpp
@@ -1,4 +1,11 @@
-#include <algorithm>
-#include <format>
-#include <iostream>
-#include <optional>
+#pragma once
+
+// clangd: https://clangd.llvm.org/guides/include-cleaner
+// suppress the warning with // IWYU pragma: keep
+
+#include <algorithm> // IWYU pragma: keep
+#include <array>     // IWYU pragma: keep
+#include <format>    // IWYU pragma: keep
+#include <iostream>  // IWYU pragma: keep
+#include <string>    // IWYU pragma: keep
+#include <vector>    // IWYU pragma: keep
