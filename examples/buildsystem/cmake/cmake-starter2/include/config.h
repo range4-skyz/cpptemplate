a36283c46f41548abfe6d18cb7d4910fe3769c3f
range4-skyz <sxy2367221250@163.com>
2025-06-15 23:37:22 +0800

    examples: add pch in cmake-starter2

---
+++ examples/buildsystem/cmake/cmake-starter2/include/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define VERSION         "1.0.0"
+#define GIT_COMMIT_HASH "4e2aa6d"
---
+++ examples/buildsystem/cmake/cmake-starter2/src/pch/cpp_pch.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// clangd: https://clangd.llvm.org/guides/include-cleaner
+// suppress the warning with // IWYU pragma: keep
+
+#include <algorithm> // IWYU pragma: keep
+#include <array>     // IWYU pragma: keep
+#include <format>    // IWYU pragma: keep
+#include <iostream>  // IWYU pragma: keep
+#include <string>    // IWYU pragma: keep
+#include <vector>    // IWYU pragma: keep
