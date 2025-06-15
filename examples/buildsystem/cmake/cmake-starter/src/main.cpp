b52513cba784c820be40d503da1009804ff97a9d
range4-skyz <sxy2367221250@163.com>
2025-06-15 20:43:08 +0800

    examples: update cmake-starter

--- examples/buildsystem/cmake/cmake-starter/src/main.cpp
+++ examples/buildsystem/cmake/cmake-starter/src/main.cpp
@@ -2,9 +2,10 @@
 #include <format>
 #include <iostream>
 
-int main() {
-    std::cout << std::format(
-        "Project version: {}.{}.{}", PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH)
-              << '\n';
+auto main() -> int {
+    std::cout << std::format("Project version: {}.{}.{}\n",
+                             PROJECT_VERSION_MAJOR,
+                             PROJECT_VERSION_MINOR,
+                             PROJECT_VERSION_PATCH);
     std::cout << std::format("Last git commit: {}", GIT_COMMIT_HASH);
 }
