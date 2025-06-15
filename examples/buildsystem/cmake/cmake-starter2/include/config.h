03950a6ad268d41022327a32a4929ecaac7f3002
range4-skyz <sxy2367221250@163.com>
2025-06-16 01:20:02 +0800

    examples: update cmake-starter2

--- examples/buildsystem/cmake/cmake-starter2/include/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define VERSION         "1.0.0"
-#define GIT_COMMIT_HASH "4e2aa6d"
--- examples/buildsystem/cmake/cmake-starter2/src/main.cpp
+++ examples/buildsystem/cmake/cmake-starter2/src/main.cpp
@@ -8,7 +8,7 @@ auto main(int /*argc*/, char * /*argv*/[]) -> int {
     // std::cout << std::format("Project version: {}\n", VERSION);
 
     fmt::print(fg(fmt::color::green), "Last git commit: {}\n", GIT_COMMIT_HASH);
-    fmt::print(fg(fmt::color::cyan) | fmt::emphasis::italic, "Project version: {}\n", VERSION);
+    fmt::print(fg(fmt::color::cyan) | fmt::emphasis::italic, "Project version: {}\n", PROJECT_VERSION);
 
     return 0;
 }
